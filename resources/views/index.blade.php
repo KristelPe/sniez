c41ba35ca3540fb106987f458dc581163f4e246f
Kristel <kristel_pire@hotmail.com>
2017-12-16 18:06:16 +0100

    added form to view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>QrCode</title>
+    <style>
+    </style>
+</head>
+<body>
+
+    <form action="{{ action('TestController@index') }}" method="post" enctype="multipart/form-data">
+        {{ csrf_field() }}
+        <input name="qr" type="file" accept="image/*" capture="camera">
+        <button>send</button>
+    </form>
+
+</body>
+</html>
