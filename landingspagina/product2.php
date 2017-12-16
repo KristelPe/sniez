8cd8ba7373f1351b9838e31b8f2c57c6056c22ab
Helena Sollie <helena.sollie@gmail.com>
2017-12-16 17:43:18 +0100

    productpages_firstpart

---
+++ landingspagina/product2.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Sniez | Spirelli pasta</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style_product.css">
+
+    <!-- Font Lato -->
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+
+</head>
+<body>
+
+<header>
+
+</header>
+
+<div id="product">
+
+    <h2>Gescand product :</h2>
+    <div class="product_info">
+
+        <img src="http://www2.soubry.be/sites/soubry.aegir/files/styles/b2c_product_detail/public/products/Spirelli.png" alt="product">
+
+        <div class="product_title">
+
+            <h3>Spirelli pasta</h3>
+            <p>Soubry</p>
+
+            <div class="product_safety">
+
+                <div id="safety"></div>
+                <div class="safety_title">Pas op!</div>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
