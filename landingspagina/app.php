28c3b9c5a7e6742b94359d9c1b6490cfe01a80a6
Helena Sollie <helena.sollie@gmail.com>
2017-12-16 19:28:21 +0100

    Added new view for scanning

---
+++ landingspagina/app.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Sniez | Nutella</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style_product.css">
+
+    <!-- Font Lato -->
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+
+</head>
+<body>
+
+<header>
+
+</header>
+
+<div id="product">
+
+    <a id="qrlink">Scan met een QR App je producten!</a>
+    <p id="qrlink_info">Zo weet je zeker of de producten veilig voor je zijn.</p>
+
+</div>
+
+</body>
+</html>
