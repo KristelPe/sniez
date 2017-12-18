0f69b95dbbb12f9a76e46dfd0241ad22fccfbdb8
Helena Sollie <helena.sollie@gmail.com>
2017-12-18 09:18:08 +0100

    Added OG for Facebookshare

--- landingspagina/product2.php
+++ landingspagina/product2.php
@@ -11,6 +11,11 @@
 
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 
+    <!-- Favicon -->
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
 </head>
 <body>
 
