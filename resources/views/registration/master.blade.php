c836101bde1a4f8e229d5511ab7660fd5d241127
Helena Sollie <helena.sollie@gmail.com>
2018-02-24 10:07:14 +0100

    Added layout screen - mobile

--- resources/views/registration/master.blade.php
+++ resources/views/registration/master.blade.php
@@ -3,11 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width,height=device-height initial-scale=1">
+
 
         <title>Sniez | We make sure you won't</title>
 
         <!-- Fonts -->
+
 
 
     </head>
--- resources/views/registration/partials/login.blade.php
+++ resources/views/registration/partials/login.blade.php
@@ -2,30 +2,81 @@
 
 <style>
 
-    .login {
+body {
 
-        margin-top: 100px;
-        margin-bottom: 100px;
-        font-family: 'Lato', sans-serif;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 80vh;
+    max-height: 90vh;
+
+    -webkit-animation-name: changeBG;
+    -webkit-animation-duration: 50s;
+    -webkit-animation-iteration-count: infinite;
+    -webkit-animation-timing-function: linear;
+}
+
+.login {
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-top: 130px;
+}
+
+#login-logo {
+
+    background-color: #E55266;
+    padding: 4em 2.2em;
+    border-radius: 100%;
+    width: 7em;
+
+    align-self: center;
+}
+
+#login-logo img{
+
+    width: 7em;
+}
+
+#login-btn {
+
+    background-color: #A0D1E6;
+    width: 12em;
+    align-self: center;
+
+    margin-top: 50px;
+}
+
+#login-btn a {
+
+    font-family: 'Lato', sans-serif;
+    font-weight: 100;
+    font-size: 0.9em;
+    text-decoration: none;
+    color: white;
+    text-align: center;
+}
+
+#login-btn span {
+
+    color: #61A0BB;
+}
+
+@-webkit-keyframes changeBG {
+    0% {
+        background-image: url("images/backgrounds/bg2.jpg");
+        background-position: right;
     }
-
-    #login a {
-
-        text-decoration: none;
+    50% {
+        background-image: url("images/backgrounds/bg1.jpg");
+        background-position: left;
     }
-
-    #login a p {
-
-
-        color: white;
-        text-align: center;
-        padding-top: 25px;
-        margin-left: 37.5%;
-        border-radius: 20px;
-        background-color: #7184ff;
-        width: 20%;
-        height: 50px;
+    100% {
+        background-image: url("images/backgrounds/bg3.jpg");
+        background-position: right;
     }
+}
+
 
 
 </style>
@@ -34,8 +85,14 @@
 
     <div class="login">
 
-        <div id="login">
-            <a href="/login/facebook"><p>Login with Facebook</p></a>
+        <div id="login-logo">
+
+            <img src="/images/logo/logo_ei.png" alt="logo">
+
+        </div>
+
+        <div id="login-btn">
+            <a href="/login/facebook"><p>Login met <span>Facebook</span></p></a>
 
         </div>
 
