fb9285c8f08e295db079612e51b9a74ffcd74421
PieterSlangen <Pieterslangen@live.nl>
2018-05-09 14:58:12 +0200

    Footer/scan and nav change

--- resources/views/scan/scan.blade.php
+++ resources/views/scan/scan.blade.php
@@ -2,6 +2,39 @@
 @section('content')
 
 <style>
+    
+    #scan_header {
+        width: 100%;
+        height: 12em;
+        background-image: url("images/backgrounds/bg_profile.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        z-index: -2;
+    }
+
+    #scan_avatar {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    #scan_avatar img {
+        width: 100px;
+        height: 100px;
+        border-radius: 100%;
+        border: 5px solid white;
+        align-self: center;
+        margin-top: -50px;
+        -webkit-box-shadow: 0 2px 4px lightgrey;
+        -moz-box-shadow: 0 2px 4px lightgrey;
+        box-shadow: 0 2px 4px lightgrey;
+    }
+
+    #scan_avatar h1 {
+        text-align: center;
+        font-size: 1em;
+        letter-spacing: 1px;
+    }
     .search{
         width: 200px;
         margin-left: auto;
@@ -76,6 +109,13 @@
         }
     }
 </style>
+
+<div id="scan_header"></div>
+
+<div id="scan_avatar">
+    <img src="{{$user->avatar}}" alt="avatar">
+    <h1>{{$user->name}}</h1>
+</div>
 
 <div class="scan">
     <div class="search">
