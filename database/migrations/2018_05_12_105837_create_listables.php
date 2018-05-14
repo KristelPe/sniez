d908cfae9ea8bc8170c59a5d7a47add4941aa9f3
Helena Sollie <helena.sollie@gmail.com>
2018-05-14 10:57:05 +0200

    changed name to get error migration:refresh away

--- database/migrations/2018_05_12_105837_create_listables.php
+++ database/migrations/2018_05_12_105837_create_listables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatListables extends Migration
+class CreateListables extends Migration
 {
     /**
      * Run the migrations.
